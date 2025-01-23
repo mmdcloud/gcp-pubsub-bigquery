0636e38ee3664687db796d2760a90a90646a8f88
Mohit Dixit <148881191+mmdcloud@users.noreply.github.com>
2025-01-23 09:39:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+module "im-workspace" {
+ source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
+ version = "~> 7.0"
+
+ project_id = nodal-talon-445602-m1
+ deployment_id = projects/nodal-talon-445602-m1/locations/us-central1/deployments/pubsub-bq
+ im_deployment_repo_uri = https://github.com/mmdcloud/gcp-pubsub-bigquery
+ im_deployment_ref = master
+
+ github_app_installation_id = 58404554
+ github_personal_access_token = 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
+}
 
 data "google_project" "project" {}
 
